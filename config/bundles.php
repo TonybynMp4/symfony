916b9b0ec2fb9fae2b3c74f96fe1d16eafb87ee7
hurcle <alexandre.peneau@gmail.com>
2021-02-16 15:01:35 +0100

    gestion asset remplacé par webpack

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Connectx\EntityAngularBundle\EntityAngularBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
