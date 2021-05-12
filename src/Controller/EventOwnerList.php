923bbbebc17c28170eb3b05b9b16e2b99caaa2ee
hurcle <alexandre.peneau@gmail.com>
2021-05-12 12:32:41 +0200

    gestion du listing private et public des events

--- src/Controller/EventOwnerList.php
+++ src/Controller/EventOwnerList.php
@@ -21,6 +21,15 @@ class EventOwnerList
         $userId = $request->attributes->get("userId");
         $type = $request->attributes->get("type");
 
+        switch ($type) {
+            case "pub":
+                $type = false;
+                break;
+            case "priv":
+                $type = true;
+                break;
+        }
+
         return $this->em->getRepository(Event::class)->findBy(["owner" => $userId, "type" => $type]);
     }
 }
